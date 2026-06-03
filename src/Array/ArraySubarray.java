eb4ad09d42235a3dc2286ba00e9c2682e71ed8ad
Prinsipethani <prinsipethani@gmail.com>
2026-06-03 07:49:23 +0530

    Updates

--- src/Array/ArraySubarray.java
+++ src/Array/ArraySubarray.java
@@ -9,6 +9,18 @@ public class ArraySubarray {
         }
         System.out.println();
     }
+    public static void printSubarrays(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
 
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -25,6 +37,9 @@ public class ArraySubarray {
                 print(arr, i, j);
             }
         }
+
+        int[] nums = {1, 2, 3};
+        printSubarrays(nums);
 
         sc.close();
     }
