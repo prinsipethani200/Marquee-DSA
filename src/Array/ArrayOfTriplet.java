854cf2c51d5194c72a9cc95e10ef1cf954b2b249
Prinsipethani <prinsipethani@gmail.com>
2026-06-04 11:01:04 +0530

    Updates

--- src/Array/ArrayOfTriplet.java
+++ src/Array/ArrayOfTriplet.java
@@ -3,20 +3,37 @@ package Array;
 import java.util.Scanner;
 
 public class ArrayOfTriplet {
-    static void Triplet(int n, int[] arr,int rasio){
-        boolean found = false;
-        for(int i = 0 ; i< n ; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (arr[j] == arr[i] * rasio && arr[k] == arr[j] * rasio) {
-                        System.out.println("Triplet = [" + arr[i] + " , " + arr[j] + " , " + arr[k] + "]");
-                        found = true;
+//    static void Triplet(int n, int[] arr,int rasio){
+//        boolean found = false;
+//        for(int i = 0 ; i< n ; i++) {
+//            for (int j = i + 1; j < n; j++) {
+//                for (int k = j + 1; k < n; k++) {
+//                    if (arr[j] == arr[i] * rasio && arr[k] == arr[j] * rasio) {
+//                        System.out.println("[" + arr[i] + " , " + arr[j] + " , " + arr[k] + "]");
+//                        found = true;
+//                    }
+//                }
+//            }
+//        }//1 2 6 18 54
+//        if (!found) {
+//            System.out.println("No Triplet Found!");
+//        }
+//    }
+
+    static void triplet(int[] arr,int n){
+        System.out.println("");
+        for(int j = 1 ; j < n ; j++){
+            int i = j + 1;
+            while(i >= 0){
+                int k = j + 1;
+                while(k < n){
+                    if(arr[j] * arr[j] == arr[i] * arr[k]){
+                        System.out.println("["+arr[i]+" "+arr[j]+" "+arr[k]+"]");
                     }
+                    k++;
                 }
+                i--;
             }
-        }
-        if (!found) {
-            System.out.println("No Triplet Found!");
         }
     }
     static void main() {
@@ -26,8 +43,9 @@ public class ArrayOfTriplet {
         for(int i = 0 ; i < n ; i++){
             arr[i] = sc.nextInt();
         }
-        int rasio = sc.nextInt();
+        //int rasio = sc.nextInt();
 
-        Triplet(n,arr,rasio);
+        //Triplet(n,arr,rasio);
+        triplet(arr,n);
     }
 }
